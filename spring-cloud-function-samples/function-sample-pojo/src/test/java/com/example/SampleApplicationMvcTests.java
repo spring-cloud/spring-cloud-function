8cf3d7bc5ae6a736c6f0831e418b3d1937f6a8b1
Dave Syer <dsyer@pivotal.io>
2018-04-24 16:06:59 +0100

    Add some WebMvcTest support
    Fixes gh-156

---
+++ spring-cloud-function-samples/function-sample-pojo/src/test/java/com/example/SampleApplicationMvcTests.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2016-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+/**
+ * @author Dave Syer
+ */
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class SampleApplicationMvcTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void words() throws Exception {
+		MvcResult mvcResult = mockMvc
+				.perform(get("/words").accept(MediaType.APPLICATION_JSON)).andReturn();
+		mockMvc.perform(asyncDispatch(mvcResult))
+				.andExpect(content().string("[{\"value\":\"foo\"},{\"value\":\"bar\"}]"));
+	}
+
+}
