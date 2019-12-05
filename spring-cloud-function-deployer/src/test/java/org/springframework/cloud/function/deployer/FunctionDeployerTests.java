fde045a7c7507906143e16724e9ba84bee9dd99d
Oleg Zhurakousky <ozhurakousky@pivotal.io>
2019-12-05 19:43:18 +0100

    Modified multi-in/out deployer test to work with POJOs

--- spring-cloud-function-deployer/src/test/java/org/springframework/cloud/function/deployer/FunctionDeployerTests.java
+++ spring-cloud-function-deployer/src/test/java/org/springframework/cloud/function/deployer/FunctionDeployerTests.java
@@ -262,32 +262,18 @@ public class FunctionDeployerTests {
 		ApplicationContext context = SpringApplication.run(DeployerApplication.class, args);
 		FunctionCatalog catalog = context.getBean(FunctionCatalog.class);
 
-		Function<Tuple2<Flux<Message<byte[]>>, Flux<Message<byte[]>>>, Tuple2<Flux<Message<byte[]>>, Flux<Message<byte[]>>>> function =
-				catalog.lookup("fn", "application/json", "application/json");
+		Function<Tuple2<Flux<Message<byte[]>>, Flux<Message<byte[]>>>, Flux<Message<byte[]>>> multiInputFunction = catalog
+				.lookup("fn", "application/json");
 
-		Message<byte[]> msg1 = MessageBuilder.withPayload("\"one\"".getBytes()).build();
-		Message<byte[]> msg2 = MessageBuilder.withPayload("\"two\"".getBytes()).build();
-		Flux<Message<byte[]>> inputOne = Flux.just(msg1, msg2);
+		Message<byte[]> carEventMessage = MessageBuilder.withPayload("{\"carEvent\":\"CAR IS BUILT\"}".getBytes()).build();
+		Message<byte[]> checkoutEventMessage = MessageBuilder.withPayload("{\"checkoutEvent\":\"CAR IS CHECKED OUT\"}".getBytes()).build();
+		Flux<Message<byte[]>> carEventStream = Flux.just(carEventMessage);
+		Flux<Message<byte[]>> checkoutEventStream = Flux.just(checkoutEventMessage);
 
-		Message<byte[]> msgInt1 = MessageBuilder.withPayload("\"1\"".getBytes()).build();
-		Message<byte[]> msgInt2 = MessageBuilder.withPayload("\"2\"".getBytes()).build();
-		Flux<Message<byte[]>> inputTwo = Flux.just(msgInt1, msgInt2);
+		Flux<Message<byte[]>> result = multiInputFunction.apply(Tuples.of(carEventStream, checkoutEventStream));
 
-		Tuple2<Flux<Message<byte[]>>, Flux<Message<byte[]>>> result = function.apply(Tuples.of(inputOne, inputTwo));
-		List<String> result1 = new ArrayList<>();
-		List<String> result2 = new ArrayList<>();
-		result.getT1().subscribe(message -> {
-			result1.add(new String(message.getPayload()));
-		});
-		result.getT2().subscribe(message -> {
-			result2.add(new String(message.getPayload()));
-		});
-
-		assertThat(result1.get(0)).isEqualTo("1.5");
-		assertThat(result1.get(1)).isEqualTo("2.0");
-
-		assertThat(result2.get(0)).isEqualTo("\"one\"");
-		assertThat(result2.get(1)).isEqualTo("\"two\"");
+		byte[] resutBytes = result.blockFirst().getPayload();
+		assertThat(resutBytes).isEqualTo("{\"orderEvent\":\"CartEvent: CAR IS BUILT- CheckoutEvent: CAR IS CHECKED OUT\"}".getBytes());
 	}
 
 	/*
