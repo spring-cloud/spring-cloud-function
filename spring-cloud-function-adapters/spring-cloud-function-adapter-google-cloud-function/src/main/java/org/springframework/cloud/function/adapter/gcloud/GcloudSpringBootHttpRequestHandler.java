54846d703dd1a0b873ba58720149c87ae05e66dc
dsolomakha <dsolomakha@google.com>
2020-02-26 12:30:00 -0500

    initial commit

---
+++ spring-cloud-function-adapters/spring-cloud-function-adapter-google-cloud-function/src/main/java/org/springframework/cloud/function/adapter/gcloud/GcloudSpringBootHttpRequestHandler.java
@@ -0,0 +1,167 @@
+/*
+ * Copyright 2012-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.function.adapter.gcloud;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import com.google.cloud.functions.Context;
+import com.google.cloud.functions.HttpRequest;
+import com.google.cloud.functions.HttpResponse;
+import com.google.gson.Gson;
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Flux;
+
+import org.springframework.cloud.function.context.AbstractSpringFunctionAdapterInitializer;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.GenericMessage;
+
+/**
+
+ */
+public class GcloudSpringBootHttpRequestHandler<O>
+		extends AbstractSpringFunctionAdapterInitializer<Context> {
+
+	Gson gson = new Gson();
+
+	public GcloudSpringBootHttpRequestHandler() {
+		super();
+	}
+
+	public GcloudSpringBootHttpRequestHandler(Class<?> configurationClass) {
+		super(configurationClass);
+	}
+
+	protected Object convert(HttpRequest event) throws IOException {
+		BufferedReader br = event.getReader();
+		StringBuilder sb = new StringBuilder();
+		String line;
+		while ((line = br.readLine()) != null) {
+			sb.append(line).append("\n");
+		}
+
+		if (functionAcceptsMessage()) {
+			Map headers = event.getHeaders();
+			return new GenericMessage<String>(sb.toString(), headers);
+		}
+		else {
+			return sb.toString();
+		}
+
+	}
+
+	protected boolean functionAcceptsMessage() {
+		return this.getInspector().isMessage(function());
+	}
+
+	@SuppressWarnings("unchecked")
+	protected <T> T result(Object input, Publisher<?> output) {
+		List<T> result = new ArrayList<>();
+		for (Object value : Flux.from(output).toIterable()) {
+			result.add((T) convertOutput(value));
+		}
+		if (isSingleValue(input) && result.size() == 1) {
+			return result.get(0);
+		}
+		return (T) result;
+	}
+	//
+	// protected boolean acceptsInput() {
+	// 	return !this.getInspector().getInputType(function()).equals(Void.class);
+	// }
+	//
+	// protected boolean returnsOutput() {
+	// 	return !this.getInspector().getOutputType(function()).equals(Void.class);
+	// }
+
+	private boolean isSingleValue(Object input) {
+		return !(input instanceof Collection);
+	}
+
+	private Flux<?> extract(Object input) {
+		// if (input instanceof Collection) {
+		// 	return Flux.fromIterable((Iterable<?>) input);
+		// }
+		return Flux.just(input);
+	}
+
+	// protected Object convertEvent(E event) {
+	// 	return event;
+	// }
+
+
+	public void handleRequest(HttpRequest httpRequest, HttpResponse httpResponse) throws Exception {
+		initialize(new TestExecutionContext("abc"));
+
+		Publisher<?> output = apply(extract(convert(httpRequest)));
+		BufferedWriter writer = httpResponse.getWriter();
+		writer.write(gson.toJson((Object) result(httpRequest, output)));
+		writer.flush();
+	}
+
+	protected O convertOutput(Object output) {
+		if (functionReturnsMessage(output)) {
+			Message<?> message = (Message<?>) output;
+			return (O) message.getPayload();
+			// for (Map.Entry<String, Object> entry : message.getHeaders().entrySet()) {
+			// 	builder = builder.header(entry.getKey(), entry.getValue().toString());
+			// }
+			// return builder.build();
+		}
+		else {
+			return (O) output;
+		}
+	}
+
+
+	protected boolean functionReturnsMessage(Object output) {
+		return output instanceof Message;
+	}
+}
+class TestExecutionContext implements Context {
+
+	private String name;
+
+	TestExecutionContext(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String eventId() {
+		return null;
+	}
+
+	@Override
+	public String timestamp() {
+		return null;
+	}
+
+	@Override
+	public String eventType() {
+		return null;
+	}
+
+	@Override
+	public String resource() {
+		return null;
+	}
+}
---
+++ spring-cloud-function-adapters/spring-cloud-function-adapter-google-cloud-function/src/test/java/org/springframework/cloud/function/adapter/gcloud/GcloudSpringBootHttpRequestHandlerTests.java
@@ -0,0 +1,355 @@
+/*
+ * Copyright 2019-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.function.adapter.gcloud;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+import com.google.cloud.functions.HttpRequest;
+import com.google.cloud.functions.HttpResponse;
+import com.google.gson.Gson;
+import org.junit.After;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import org.springframework.cloud.function.context.config.ContextFunctionCatalogAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.GenericMessage;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author
+ */
+public class GcloudSpringBootHttpRequestHandlerTests {
+	HttpRequest request =  Mockito.mock(HttpRequest.class);
+	HttpResponse response = Mockito.mock(HttpResponse.class);
+
+	private GcloudSpringBootHttpRequestHandler<?> handler = null;
+	public static final Gson GSON = new Gson();
+
+	<O> GcloudSpringBootHttpRequestHandler<O> handler(Class<?> config) {
+		GcloudSpringBootHttpRequestHandler<O> handler =
+			new GcloudSpringBootHttpRequestHandler<O>(config);
+		this.handler = handler;
+		return handler;
+	}
+
+	@Test
+	public void testWithBody() throws Exception {
+		GcloudSpringBootHttpRequestHandler<Foo> handler = handler(FunctionMessageBodyConfig.class);
+
+		StringReader foo = new StringReader(GSON.toJson(new Foo("foo")));
+		when(request.getReader()).thenReturn(new BufferedReader(foo));
+
+		StringWriter writer = new StringWriter();
+		// when(response.getWriter()).thenReturn(new BufferedWriter(writer));
+		handler.handleRequest(request, new HttpResponseImpl(new BufferedWriter(writer)));
+
+		assertThat(GSON.fromJson(writer.toString(), Bar.class)).isEqualTo(new Bar("FOO"));
+	}
+
+	@Test
+	public void testWithRequestParameters() throws Exception {
+		GcloudSpringBootHttpRequestHandler<Foo> handler = handler(FunctionMessageBodyConfig.class);
+
+		StringReader foo = new StringReader(GSON.toJson(new Foo("foo")));
+		when(request.getReader()).thenReturn(new BufferedReader(foo));
+		when(request.getUri()).thenReturn("http://localhost:8080/pathValue");
+		when(request.getHeaders())
+			.thenReturn(Collections.singletonMap("test-header", Collections.singletonList("headerValue")));
+		when(request.getQueryParameters())
+			.thenReturn(Collections.singletonMap("query", Collections.singletonList("queryValue")));
+		when(request.getMethod()).thenReturn("GET");
+
+		StringWriter writer = new StringWriter();
+		HttpResponseImpl response = new HttpResponseImpl(new BufferedWriter(writer));
+		handler.handleRequest(request, response);
+
+		// verify(this.response, times(1)).setStatusCode(eq(200));
+		assertThat(response.statusCode).isEqualTo(200);
+		assertThat(response.headers.get("path")).containsExactly("/pathValue");
+		assertThat(response.headers.get("query")).containsExactly("queryValue");
+		assertThat(response.headers.get("test-header")).containsExactly("headerValue");
+		assertThat(GSON.fromJson(writer.toString(), Bar.class)).isEqualTo(new Bar("FOO"));
+	}
+	//
+	// @Test
+	// public void testWithEmptyBody() {
+	// 	AzureSpringBootHttpRequestHandler<Foo> handler = handler(
+	// 			FunctionMessageConsumerConfig.class);
+	// 	HttpRequestMessageStub<Foo> request = new HttpRequestMessageStub<Foo>();
+	//
+	// 	HttpResponseMessage response = handler.handleRequest(request,
+	// 			new TestExecutionContext("uppercase"));
+	//
+	// 	assertThat(response.getStatusCode()).isEqualTo(200);
+	// 	Bar body = (Bar) response.getBody();
+	// 	assertThat(body).isNull();
+	// }
+
+	@After
+	public void close()  {
+		if (this.handler != null) {
+			this.handler.close();
+		}
+	}
+
+	@Configuration
+	@Import({ ContextFunctionCatalogAutoConfiguration.class })
+	protected static class FunctionMessageBodyConfig {
+
+		@Bean
+		public Function<Message<Foo>, Message<Bar>> function() {
+			return (foo -> {
+				Map<String, Object> headers = new HashMap<>();
+				return new GenericMessage<>(
+						new Bar(foo.getPayload().getValue().toUpperCase()), headers);
+			});
+		}
+
+	}
+
+	@Configuration
+	@Import({ ContextFunctionCatalogAutoConfiguration.class })
+	protected static class FunctionMessageEchoReqParametersConfig {
+
+		@Bean
+		public Function<Message<Void>, Message<Bar>> function() {
+			return (message -> {
+				Map<String, Object> headers = new HashMap<>();
+				headers.put("path", message.getHeaders().get("path"));
+				headers.put("query", message.getHeaders().get("query"));
+				headers.put("test-header", message.getHeaders().get("test-header"));
+				headers.put("httpMethod", message.getHeaders().get("httpMethod"));
+				return new GenericMessage<>(new Bar("body"), headers);
+			});
+		}
+
+	}
+
+	@Configuration
+	@Import({ ContextFunctionCatalogAutoConfiguration.class })
+	protected static class FunctionMessageConsumerConfig {
+
+		@Bean
+		public Consumer<Message<Foo>> function() {
+			return (foo -> {
+			});
+		}
+
+	}
+	//
+	// class BuilderStub implements Builder {
+	//
+	// 	private HttpStatusType status;
+	// 	private Map<String, String> headers = new HashMap<>();
+	// 	private Object body;
+	//
+	// 	@Override
+	// 	public Builder status(HttpStatusType status) {
+	// 		this.status = status;
+	// 		return this;
+	// 	}
+	//
+	// 	@Override
+	// 	public Builder header(String key, String value) {
+	// 		headers.put(key, value);
+	// 		return this;
+	// 	}
+	//
+	// 	@Override
+	// 	public Builder body(Object body) {
+	// 		this.body = body;
+	// 		return this;
+	// 	}
+	//
+	// 	@Override
+	// 	public HttpResponseMessage build() {
+	// 		return new HttpResponseMessageStub(this.status, this.headers, this.body);
+	// 	}
+	//
+	// }
+	//
+	// class HttpResponseMessageStub implements HttpResponseMessage {
+	//
+	// 	private HttpStatusType status;
+	// 	private Map<String, String> headers = new HashMap<>();
+	// 	private Object body;
+	//
+	// 	HttpResponseMessageStub(HttpStatusType status, Map<String, String> headers,
+	// 			Object body) {
+	// 		this.status = status;
+	// 		this.headers = headers;
+	// 		this.body = body;
+	// 	}
+	//
+	// 	@Override
+	// 	public HttpStatusType getStatus() {
+	// 		return this.status;
+	// 	}
+	//
+	// 	@Override
+	// 	public String getHeader(String key) {
+	// 		return this.headers.get(key);
+	// 	}
+	//
+	// 	@Override
+	// 	public Object getBody() {
+	// 		return this.body;
+	// 	}
+	//
+	// }
+}
+
+class Foo {
+
+	private String value;
+
+	Foo() {
+	}
+
+	Foo(String value) {
+		this.value = value;
+	}
+
+	public String lowercase() {
+		return this.value.toLowerCase();
+	}
+
+	public String uppercase() {
+		return this.value.toUpperCase();
+	}
+
+	public String getValue() {
+		return this.value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+}
+
+class Bar {
+
+	private String value;
+
+	Bar() {
+	}
+
+	Bar(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return this.value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Bar bar = (Bar) o;
+		return Objects.equals(getValue(), bar.getValue());
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(getValue());
+	}
+}
+
+class HttpResponseImpl implements HttpResponse {
+
+	int statusCode;
+
+	String contentType;
+
+	BufferedWriter writer;
+
+	HttpResponseImpl(BufferedWriter writer) {
+		this.writer = writer;
+	}
+
+	Map<String, List<String>> headers = new HashMap<>();
+
+	@Override
+	public void setStatusCode(int code) {
+		statusCode = code;
+	}
+
+	@Override
+	public void setStatusCode(int code, String message) {
+		statusCode = code;
+	}
+
+	@Override
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
+
+	@Override
+	public Optional<String> getContentType() {
+		return Optional.ofNullable(contentType);
+	}
+
+	@Override
+	public void appendHeader(String header, String value) {
+		headers.getOrDefault(header, new ArrayList<>()).add(value);
+	}
+
+	@Override
+	public Map<String, List<String>> getHeaders() {
+		return headers;
+	}
+
+	@Override
+	public OutputStream getOutputStream() throws IOException {
+		throw new RuntimeException("unsuported!");
+	}
+
+	@Override
+	public BufferedWriter getWriter() throws IOException {
+		return writer;
+	}
+}
