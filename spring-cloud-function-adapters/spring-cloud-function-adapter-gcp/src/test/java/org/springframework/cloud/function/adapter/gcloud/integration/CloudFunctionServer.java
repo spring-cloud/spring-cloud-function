7475cbeab61f83ae6ab4238a404eb56f8db61d8e
dzou <dzou@users.noreply.github.com>
2020-03-20 13:42:46 -0400

    Use awaitility

--- spring-cloud-function-adapters/spring-cloud-function-adapter-gcp/src/test/java/org/springframework/cloud/function/adapter/gcloud/integration/CloudFunctionServer.java
+++ spring-cloud-function-adapters/spring-cloud-function-adapter-gcp/src/test/java/org/springframework/cloud/function/adapter/gcloud/integration/CloudFunctionServer.java
@@ -16,8 +16,14 @@
 
 package org.springframework.cloud.function.adapter.gcloud.integration;
 
+import java.util.concurrent.TimeUnit;
+
 import com.google.cloud.functions.invoker.runner.Invoker;
 import org.junit.rules.ExternalResource;
+
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.awaitility.Awaitility.await;
 
 /**
  * Test rule for starting the Cloud Function server.
@@ -78,7 +84,11 @@ public class CloudFunctionServer extends ExternalResource {
 		this.serverThread.start();
 
 		// Wait for the server to start up.
-		Thread.sleep(1000);
+
+		TestRestTemplate template = new TestRestTemplate();
+		await().atMost(5, TimeUnit.SECONDS)
+			.until(() -> template.postForEntity(
+				"http://localhost:" + this.port, "test", String.class).getStatusCode().is2xxSuccessful());
 	}
 
 	@Override
