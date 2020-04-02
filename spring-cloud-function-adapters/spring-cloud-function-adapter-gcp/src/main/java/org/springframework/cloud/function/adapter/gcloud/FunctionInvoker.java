24e5b13adcda6e7330cb141d0b26a8b5704d19e3
Mike Eltsufin <meltsufin@google.com>
2020-04-02 11:56:45 -0400

    Adds Readme to GCP sample app
    Also, fixing a bug in the default constructor of the invoker.
    
    Fixes: #478.

--- spring-cloud-function-adapters/spring-cloud-function-adapter-gcp/src/main/java/org/springframework/cloud/function/adapter/gcloud/FunctionInvoker.java
+++ spring-cloud-function-adapters/spring-cloud-function-adapter-gcp/src/main/java/org/springframework/cloud/function/adapter/gcloud/FunctionInvoker.java
@@ -46,13 +46,18 @@ public class FunctionInvoker
 
 	public FunctionInvoker() {
 		super();
+		init();
 	}
 
 	public FunctionInvoker(Class<?> configurationClass) {
 		super(configurationClass);
+		init();
+	}
+
+	private void init() {
 		System.setProperty("spring.http.converters.preferred-json-mapper", "gson");
 		Thread.currentThread() //TODO investigate if it is necessary
-			.setContextClassLoader(FunctionInvoker.class.getClassLoader());
+				.setContextClassLoader(FunctionInvoker.class.getClassLoader());
 		initialize(null);
 	}
 
