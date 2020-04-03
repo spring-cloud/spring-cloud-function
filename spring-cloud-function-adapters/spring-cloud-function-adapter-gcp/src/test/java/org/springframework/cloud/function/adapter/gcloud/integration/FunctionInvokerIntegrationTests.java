b91d6ee0082feb72ade9f0476ab73ac074d5785d
dzou <dzou@users.noreply.github.com>
2020-04-03 11:16:04 -0400

    Add GCF integration tests
    Add Integration Tests for GcfSpringBootHttpRequestHandler2
    
    fix up
    
    fix build
    
    cleanup after merge
    
    Added process-based server integration test support
    
    some more refactoring
    
    remove unneeded maven deps
    
    address Dmitry and Dans feedback

---
+++ spring-cloud-function-adapters/spring-cloud-function-adapter-gcp/src/test/java/org/springframework/cloud/function/adapter/gcloud/integration/FunctionInvokerIntegrationTests.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2012-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.function.adapter.gcloud.integration;
+
+import java.io.IOException;
+import java.util.function.Function;
+
+import org.junit.Test;
+
+import org.springframework.cloud.function.context.config.ContextFunctionCatalogAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import static org.springframework.cloud.function.adapter.gcloud.integration.LocalServerTestSupport.verify;
+
+/**
+ * Integration tests for GCF Http Functions.
+ *
+ * @author Daniel Zou
+ * @author Mike Eltsufin
+ */
+public class FunctionInvokerIntegrationTests {
+
+	@Test
+	public void testSingular() {
+		verify(CloudFunctionMainSingular.class, null, "hello", "HELLO");
+	}
+
+	@Test
+	public void testUppercase() throws InterruptedException, IOException {
+		verify(CloudFunctionMain.class, "uppercase", "hello", "HELLO");
+
+	}
+
+	@Test
+	public void testFooBar() {
+		verify(CloudFunctionMain.class, "foobar", new Foo("Hi"), new Bar("Hi"));
+	}
+
+	@Configuration
+	@Import({ ContextFunctionCatalogAutoConfiguration.class })
+	static class CloudFunctionMainSingular {
+
+		@Bean
+		Function<String, String> uppercase() {
+			return input -> input.toUpperCase();
+		}
+
+	}
+
+	@Configuration
+	@Import({ ContextFunctionCatalogAutoConfiguration.class })
+	static class CloudFunctionMain {
+
+		@Bean
+		Function<String, String> uppercase() {
+			return input -> input.toUpperCase();
+		}
+
+		@Bean
+		Function<Foo, Bar> foobar() {
+			return input -> new Bar(input.value);
+		}
+
+	}
+
+	private static class Foo {
+
+		String value;
+
+		Foo(String value) {
+			this.value = value;
+		}
+
+	}
+
+	private static class Bar {
+
+		String value;
+
+		Bar(String value) {
+			this.value = value;
+		}
+
+	}
+
+}
