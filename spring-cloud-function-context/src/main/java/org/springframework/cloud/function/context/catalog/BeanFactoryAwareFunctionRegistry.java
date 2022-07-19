c08cf7ffcb26da778d4223f3b75c1dcc26ea6846
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2022-07-20 00:04:29 +0200

    Fix typo.
    Fix typo.

--- spring-cloud-function-context/src/main/java/org/springframework/cloud/function/context/catalog/BeanFactoryAwareFunctionRegistry.java
+++ spring-cloud-function-context/src/main/java/org/springframework/cloud/function/context/catalog/BeanFactoryAwareFunctionRegistry.java
@@ -52,7 +52,7 @@ import org.springframework.messaging.converter.CompositeMessageConverter;
 import org.springframework.util.StringUtils;
 
 /**
- * Implementation of {@link FunctionRegistry} capable of discovering functioins in {@link BeanFactory}.
+ * Implementation of {@link FunctionRegistry} capable of discovering functions in {@link BeanFactory}.
  *
  * @author Oleg Zhurakousky
  */
