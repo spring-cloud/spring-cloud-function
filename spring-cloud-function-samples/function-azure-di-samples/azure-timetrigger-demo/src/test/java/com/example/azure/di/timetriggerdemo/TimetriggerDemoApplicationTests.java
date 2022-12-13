38ab29f630e716578711673f6f60940f79257313
Christian Tzolov <christian.tzolov@gmail.com>
2022-12-13 15:44:51 +0100

    Support Azure Functions DI hook
     - Extend spring-cloud-function-adapter-azure with AzureFunctionInstanceInjector registered through the  META-INF/services/com.microsoft.azure.functions.spi.inject.FunctionInstanceInjector
     - Add DI demos for HttpTrigger and TimeTrigger functions.
    
     Resolves #968

---
+++ spring-cloud-function-samples/function-azure-di-samples/azure-timetrigger-demo/src/test/java/com/example/azure/di/timetriggerdemo/TimetriggerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.azure.di.timetriggerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetriggerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
