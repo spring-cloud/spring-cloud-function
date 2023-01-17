9ec65974683650b4dc00a4c4bdcab1929c54a936
Oleg Zhurakousky <ozhurakousky@vmware.com>
2023-01-17 14:15:10 +0100

    GH-986 Fix regression to NOT attempt output conversion of Message<byte[]>
    Resolves #986

--- spring-cloud-function-context/src/main/java/org/springframework/cloud/function/context/catalog/SimpleFunctionRegistry.java
+++ spring-cloud-function-context/src/main/java/org/springframework/cloud/function/context/catalog/SimpleFunctionRegistry.java
@@ -1136,7 +1136,7 @@ public class SimpleFunctionRegistry implements FunctionRegistry {
 			}
 
 			if (convertedOutput instanceof Message) {
-				if (((Message) convertedOutput).getPayload() instanceof byte[] && ObjectUtils.isEmpty(contentType)) {
+				if (((Message) convertedOutput).getPayload() instanceof byte[]) {
 					return convertedOutput;
 				}
 				else if (isExtractPayload((Message<?>) convertedOutput, type)) {
