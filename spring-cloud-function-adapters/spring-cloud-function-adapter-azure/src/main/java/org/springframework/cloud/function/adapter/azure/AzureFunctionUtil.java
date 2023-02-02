ba1094df63c4fba84d86deb74eeb51cb9985757a
Christian Tzolov <christian.tzolov@gmail.com>
2023-02-02 16:45:15 +0100

    resolve code style issues Resolves #999

--- spring-cloud-function-adapters/spring-cloud-function-adapter-azure/src/main/java/org/springframework/cloud/function/adapter/azure/AzureFunctionUtil.java
+++ spring-cloud-function-adapters/spring-cloud-function-adapter-azure/src/main/java/org/springframework/cloud/function/adapter/azure/AzureFunctionUtil.java
@@ -14,102 +14,105 @@
  * limitations under the License.
  */
 
- package org.springframework.cloud.function.adapter.azure;
+package org.springframework.cloud.function.adapter.azure;
 
- import java.util.HashMap;
- import java.util.Map;
- import java.util.Optional;
- 
- import com.microsoft.azure.functions.ExecutionContext;
- import com.microsoft.azure.functions.HttpMethod;
- import com.microsoft.azure.functions.HttpRequestMessage;
- import org.reactivestreams.Publisher;
- import reactor.core.publisher.Flux;
- 
- import org.springframework.messaging.Message;
- import org.springframework.messaging.MessageHeaders;
- import org.springframework.messaging.support.MessageBuilder;
- 
- /**
-  * 
-  * @author Christian Tzolov
-  * @author Oleg Zhurakousky
-  * @author Chris Bono
-  * @since 4.0
-  * 
-  */
- public class AzureFunctionUtil {
- 
-     public static String EXECUTION_CONTEXT = "executionContext";
- 
-     @SuppressWarnings({ "unchecked", "rawtypes" })
-     public static <I> Object enhanceInputIfNecessary(Object input, ExecutionContext executionContext) {
-         if (input == null) { // Supplier
-             return input;
-         }
-         if (input instanceof Publisher) {
-             return Flux.from((Publisher) input).map(item -> {
-                 if (item instanceof Message) {
-                     return MessageBuilder.fromMessage((Message<I>) item)
-                             .setHeaderIfAbsent(EXECUTION_CONTEXT, executionContext).build();
-                 }
-                 else {
-                     return constructInputMessageFromItem(input, executionContext);
-                 }
-             });
-         }
-         else if (input instanceof Message) {
-             return MessageBuilder.fromMessage((Message<I>) input)
-                     .setHeaderIfAbsent(EXECUTION_CONTEXT, executionContext).build();
-         }
-         else if (input instanceof Iterable) {
-             return Flux.fromIterable((Iterable) input).map(item -> {
-                 return constructInputMessageFromItem(item, executionContext);
-             });
-         }
-         return constructInputMessageFromItem(input, executionContext);
-     }
- 
-     private static <I> Message<?> constructInputMessageFromItem(Object input, ExecutionContext executionContext) {
-         MessageBuilder<?> messageBuilder = null;
-         if (input instanceof HttpRequestMessage) {
-             HttpRequestMessage<I> requestMessage = (HttpRequestMessage<I>) input;
-             Object payload = requestMessage.getHttpMethod() != null
-                     && requestMessage.getHttpMethod().equals(HttpMethod.GET)
-                             ? requestMessage.getQueryParameters()
-                             : requestMessage.getBody();
- 
-             if (payload == null) {
-                 payload = Optional.empty();
-             }
-             messageBuilder = MessageBuilder.withPayload(payload).copyHeaders(getHeaders(requestMessage));
-         }
-         else {
-             messageBuilder = MessageBuilder.withPayload(input);
-         }
-         return messageBuilder.setHeaderIfAbsent(EXECUTION_CONTEXT, executionContext).build();
-     }
- 
-     private static <I> MessageHeaders getHeaders(HttpRequestMessage<I> event) {
-         Map<String, Object> headers = new HashMap<String, Object>();
- 
-         if (event.getHeaders() != null) {
-             headers.putAll(event.getHeaders());
-         }
-         if (event.getQueryParameters() != null) {
-             headers.putAll(event.getQueryParameters());
-         }
-         if (event.getUri() != null) {
-             headers.put("path", event.getUri().getPath());
-         }
- 
-         if (event.getHttpMethod() != null) {
-             headers.put("httpMethod", event.getHttpMethod().toString());
-         }
- 
-         headers.put("request", event.getBody());
-         return new MessageHeaders(headers);
-     }
- 
- }
- 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Flux;
+
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.support.MessageBuilder;
+
+/**
+ * @author Christian Tzolov
+ * @author Oleg Zhurakousky
+ * @author Chris Bono
+ * @since 4.0
+ */
+public final class AzureFunctionUtil {
+
+	/**
+	 * Message header key name used to store and extract the ExecutionContext.
+	 */
+	public static String EXECUTION_CONTEXT = "executionContext";
+
+	private AzureFunctionUtil() {
+	};
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static <I> Object enhanceInputIfNecessary(Object input, ExecutionContext executionContext) {
+		if (input == null) { // Supplier
+			return input;
+		}
+		if (input instanceof Publisher) {
+			return Flux.from((Publisher) input).map(item -> {
+				if (item instanceof Message) {
+					return MessageBuilder.fromMessage((Message<I>) item)
+							.setHeaderIfAbsent(EXECUTION_CONTEXT, executionContext).build();
+				}
+				else {
+					return constructInputMessageFromItem(input, executionContext);
+				}
+			});
+		}
+		else if (input instanceof Message) {
+			return MessageBuilder.fromMessage((Message<I>) input)
+					.setHeaderIfAbsent(EXECUTION_CONTEXT, executionContext).build();
+		}
+		else if (input instanceof Iterable) {
+			return Flux.fromIterable((Iterable) input).map(item -> {
+				return constructInputMessageFromItem(item, executionContext);
+			});
+		}
+		return constructInputMessageFromItem(input, executionContext);
+	}
+
+	private static <I> Message<?> constructInputMessageFromItem(Object input, ExecutionContext executionContext) {
+		MessageBuilder<?> messageBuilder = null;
+		if (input instanceof HttpRequestMessage) {
+			HttpRequestMessage<I> requestMessage = (HttpRequestMessage<I>) input;
+			Object payload = requestMessage.getHttpMethod() != null
+					&& requestMessage.getHttpMethod().equals(HttpMethod.GET)
+							? requestMessage.getQueryParameters()
+							: requestMessage.getBody();
+
+			if (payload == null) {
+				payload = Optional.empty();
+			}
+			messageBuilder = MessageBuilder.withPayload(payload).copyHeaders(getHeaders(requestMessage));
+		}
+		else {
+			messageBuilder = MessageBuilder.withPayload(input);
+		}
+		return messageBuilder.setHeaderIfAbsent(EXECUTION_CONTEXT, executionContext).build();
+	}
+
+	private static <I> MessageHeaders getHeaders(HttpRequestMessage<I> event) {
+		Map<String, Object> headers = new HashMap<String, Object>();
+
+		if (event.getHeaders() != null) {
+			headers.putAll(event.getHeaders());
+		}
+		if (event.getQueryParameters() != null) {
+			headers.putAll(event.getQueryParameters());
+		}
+		if (event.getUri() != null) {
+			headers.put("path", event.getUri().getPath());
+		}
+
+		if (event.getHttpMethod() != null) {
+			headers.put("httpMethod", event.getHttpMethod().toString());
+		}
+
+		headers.put("request", event.getBody());
+		return new MessageHeaders(headers);
+	}
+
+}
