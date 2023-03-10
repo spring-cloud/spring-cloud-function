04baab62b71d772fefdcf2d74ad717943015f0e6
Christian Tzolov <christian.tzolov@gmail.com>
2023-03-10 08:04:23 +0100

    updatee the azure web adapter

--- spring-cloud-function-adapters/spring-cloud-function-adapter-azure-web/src/test/java/org/springframework/cloud/function/adapter/azure/web/PetStoreSpringAppConfig.java
+++ spring-cloud-function-adapters/spring-cloud-function-adapter-azure-web/src/test/java/org/springframework/cloud/function/adapter/azure/web/PetStoreSpringAppConfig.java
@@ -32,7 +32,7 @@ import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
-//@SpringBootApplication
+
 @Configuration
 @Import({ PetsController.class })
 public class PetStoreSpringAppConfig {
