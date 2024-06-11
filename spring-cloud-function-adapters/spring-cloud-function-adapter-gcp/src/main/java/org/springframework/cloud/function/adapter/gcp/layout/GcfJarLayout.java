abdb1ac04fd17c0c642e229b31f82a36e6ab33ef
Lauren Wiebenga <lauren.wiebenga@species360.org>
2024-06-11 08:48:51 -0500

    Updates GcfJarLayout to write back classic loaders required for the current implementation of this project, before this change the default loaders would be used every time GcfJarLayout was invoked; also updates the pom for function-sample-gcp-http to use the latest development version of the spring cloud gcp adaptor Fixes gh-1085

--- spring-cloud-function-adapters/spring-cloud-function-adapter-gcp/src/main/java/org/springframework/cloud/function/adapter/gcp/layout/GcfJarLayout.java
+++ spring-cloud-function-adapters/spring-cloud-function-adapter-gcp/src/main/java/org/springframework/cloud/function/adapter/gcp/layout/GcfJarLayout.java
@@ -21,6 +21,7 @@ import java.io.IOException;
 import org.springframework.boot.loader.tools.CustomLoaderLayout;
 import org.springframework.boot.loader.tools.Layouts;
 import org.springframework.boot.loader.tools.LoaderClassesWriter;
+import org.springframework.boot.loader.tools.LoaderImplementation;
 import org.springframework.cloud.function.adapter.gcp.GcfJarLauncher;
 
 /**
@@ -46,7 +47,7 @@ public class GcfJarLayout extends Layouts.Jar implements CustomLoaderLayout {
 
 	@Override
 	public void writeLoadedClasses(LoaderClassesWriter writer) throws IOException {
-		writer.writeLoaderClasses();
+		writer.writeLoaderClasses(LoaderImplementation.CLASSIC);
 
 		String jarName = LAUNCHER_NAME.replaceAll("\\.", "/") + ".class";
 		writer.writeEntry(
