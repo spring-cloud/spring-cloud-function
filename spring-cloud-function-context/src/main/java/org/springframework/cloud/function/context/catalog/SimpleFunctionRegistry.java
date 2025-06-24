e14e86b44ac2294d5558028b811f5087eee54b6a
Wilson da Rocha Franca <wilsonrf@gmail.com>
2025-06-24 21:39:53 +0200

    GH-1285 Change log level to debug for failed function lookup
    Resolves #1285
    
    Signed-off-by: Wilson da Rocha Franca <wilsonrf@gmail.com>

--- spring-cloud-function-context/src/main/java/org/springframework/cloud/function/context/catalog/SimpleFunctionRegistry.java
+++ spring-cloud-function-context/src/main/java/org/springframework/cloud/function/context/catalog/SimpleFunctionRegistry.java
@@ -300,8 +300,10 @@ public class SimpleFunctionRegistry implements FunctionRegistry {
 		for (String functionName : functionNames) {
 			FunctionInvocationWrapper function = this.findFunctionInFunctionRegistrations(functionName);
 			if (function == null) {
-				logger.warn("Failed to locate function '" + functionName + "' for function definition '"
+				if (logger.isDebugEnabled()) {
+					logger.debug("Failed to locate function '" + functionName + "' for function definition '"
 						+ functionDefinition + "'. Returning null.");
+				}
 				return null;
 			}
 			else {
